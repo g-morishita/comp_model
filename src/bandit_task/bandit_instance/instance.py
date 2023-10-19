1a6b5d980788467c79357418c47263d0e25c004f
Gota Morishita <optimistic.gota@gmail.com>
2023-10-19 21:48:59 +1100

    refactor bandit instance

--- src/bandit_task/bandit_instance/instance.py
+++ src/bandit_task/bandit_instance/instance.py
@@ -6,12 +6,33 @@ from .arm import NormalArm, BernoulliArm
 
 
 class Bandit(ABC):
+    """
+    Abstract base class representing a generic bandit with multiple arms.
+
+    Methods
+    -------
+    select_choice(choice : int) -> int | float
+        Abstract method to select and execute a choice (pull an arm) to get a reward.
+    """
+
     @abstractmethod
     def select_choice(self, choice: int) -> int | float:
+        """Selects an arm (choice) and returns the corresponding reward."""
         pass
 
 
 class NormalBandit(Bandit):
+    """
+    Represents a multi-armed bandit with arms that follow a normal distribution.
+
+    Parameters
+    ----------
+    means : Sequence[int | float]
+        List of means for each arm's normal distribution.
+    sds : Sequence[int | float]
+        List of standard deviations for each arm's normal distribution.
+    """
+
     def __init__(
         self, means: Sequence[int | float], sds: Sequence[int | float]
     ) -> None:
@@ -21,34 +42,65 @@ class NormalBandit(Bandit):
                     len(means)={len(means)}, len(sds)={len(sds)}"
             )
 
-        self.arms = []
-        for mean, sd in zip(means, sds):
-            self.arms.append(NormalArm(mean, sd))
+        self.arms = [NormalArm(mean, sd) for mean, sd in zip(means, sds)]
 
     def select_choice(self, choice: int) -> float:
+        """
+        Selects a specific arm (choice) and returns the corresponding reward from a normal distribution.
+
+        Parameters
+        ----------
+        choice : int
+            The index of the arm to select.
+
+        Returns
+        -------
+        float
+            The reward value.
+        """
         if (choice < 0) or (choice >= len(self.arms)):
             raise ValueError(
-                "chosen_arm must be between 0 and len(self.arms). \
-                    {chosen_arm} is given."
+                f"choice must be between 0 and {len(self.arms) - 1}. \
+                    {choice} is given."
             )
 
         return self.arms[choice].generate_reward()
 
 
 class BernoulliBandit(Bandit):
+    """
+    Represents a multi-armed bandit with arms that follow a Bernoulli distribution.
+
+    Parameters
+    ----------
+    means : Sequence[int | float]
+        List of probabilities (mean) for each arm's Bernoulli distribution.
+    """
+
     def __init__(
         self,
         means: Sequence[int | float],
     ) -> None:
-        self.arms = []
-        for mean in means:
-            self.arms.append(BernoulliArm(mean))
+        self.arms = [BernoulliArm(mean) for mean in means]
 
-    def select_choice(self, chosen_arm: int) -> float:
-        if (chosen_arm < 0) or (chosen_arm >= len(self.arms)):
+    def select_choice(self, choice: int) -> int:
+        """
+        Selects a specific arm (choice) and returns the corresponding reward from a Bernoulli distribution.
+
+        Parameters
+        ----------
+        choice : int
+            The index of the arm to select.
+
+        Returns
+        -------
+        int
+            The reward value (0 or 1).
+        """
+        if (choice < 0) or (choice >= len(self.arms)):
             raise ValueError(
-                "chosen_arm must be between 0 and len(self.arms). \
-                    {chosen_arm} is given."
+                f"choice must be between 0 and {len(self.arms) - 1}. \
+                    {choice} is given."
             )
 
-        return self.arms[chosen_arm].generate_reward()
+        return self.arms[choice].generate_reward()
