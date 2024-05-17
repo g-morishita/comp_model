781163232e66fdf9577c7aa67dcc6dceb9d51221
Gota Morishita <optimistic.gota@gmail.com>
2024-05-17 16:59:27 +1000

    implemented information bonus q learning model

--- src/bandit_task/social_bandit_model/simulator/q_model.py
+++ src/bandit_task/social_bandit_model/simulator/q_model.py
@@ -54,7 +54,6 @@ class QSoftmaxSimulator(BaseSimulator):
         # Update the Q-value of the action.
         self.q_values[choice] = self.q_values[choice] + self.lr_own * delta
 
-
     def learn_from_partner(self, choice: int, reward: float) -> None:
         """
         Update the Q-value for partner's choice and the received reward.
@@ -70,3 +69,25 @@ class QSoftmaxSimulator(BaseSimulator):
         delta = reward - self.q_values[choice]
         # Update the Q-value of the action.
         self.q_values[choice] = self.q_values[choice] + self.lr_partner * delta
+
+
+class QSoftmaxInfoBonusSimulator(QSoftmaxSimulator):
+    def __init__(self, lr_own, lr_partner, beta, coef_info_bonus, initial_values):
+        super().__init__(lr_own, lr_partner, beta, initial_values)
+        self.n_chosen = np.ones(len(initial_values))
+        self.coef_info_bonus = coef_info_bonus
+
+    def make_choice(self) -> int:
+        # Calculate the probability of each action using the softmax function.
+        values = self.q_values * self.beta + 1 / np.sqrt(self.n_chosen) * self.coef_info_bonus
+        choice_prob = softmax(values)
+        # Randomly select an action based on its probability.
+        return np.random.choice(len(self.q_values), p=choice_prob)
+
+    def learn_from_own(self, choice: int, reward: float) -> None:
+        super().learn_from_own(choice, reward)
+        self.n_chosen[choice] += 1
+
+    def learn_from_partner(self, choice: int, reward: float) -> None:
+        super().learn_from_partner(choice, reward)
+        self.n_chosen[choice] += 1
