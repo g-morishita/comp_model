9bfad40a6f56c42dd3ef90d17ff6b9e13bf5d561
Gota Morishita <optimistic.gota@gmail.com>
2024-05-21 17:35:02 +1000

    implement bayesian model

---
+++ src/bandit_task/social_bandit_model/estimator/bayesian_model.py
@@ -0,0 +1,43 @@
+import os
+from typing import Sequence
+
+import numpy as np
+from scipy.optimize import LinearConstraint
+from scipy.special import softmax
+
+from .base import MLEstimator, HierarchicalEstimator, BayesianEstimator
+from ...type import NDArrayNumber
+
+
+class BetaModelSoftmaxWithOwnReward(MLEstimator):
+    def __init__(self):
+        super().__init__()
+
+    def neg_ll(self, params):
+        beta = params[0]
+
+        n_trials = len(self.your_choices)
+        beta_params = np.ones((n_trials, 2, self.num_choices))
+
+        for t in range(1, n_trials):
+            beta_params[t, self.your_rewards[t - 1], self.your_choices[t - 1]] += 1
+
+        values_table = beta_params[:, 1, :] / beta_params.sum(axis=1)
+        # Calculate choice probabilities using softmax function
+        choice_prob = softmax(beta * values_table, axis=1)
+
+        # Calculate negative log-likelihood using your own choices not partners!
+        chosen_prob = choice_prob[np.arange(n_trials), self.your_choices]
+        nll = -np.log(chosen_prob + 1e-8).sum()
+
+        return nll
+
+    def initialize_params(self) -> NDArrayNumber:
+        init_beta = np.random.gamma(2, 0.333)
+        return np.array([init_beta])
+
+    def constraints(self):
+        A = np.eye(1)
+        lb = np.array([0])
+        ub = [np.inf]
+        return LinearConstraint(A, lb, ub)
---
+++ src/bandit_task/social_bandit_model/simulator/bayesian_model.py
@@ -0,0 +1,38 @@
+import numpy as np
+from scipy.special import softmax
+from abc import ABC, abstractmethod
+
+
+class BaseSimulator(ABC):
+    @abstractmethod
+    def make_choice(self):
+        pass
+
+    @abstractmethod
+    def learn_from_own(self, choice: int, reward: float) -> None:
+        pass
+
+    @abstractmethod
+    def learn_from_partner(self, choice: int, reward: float) -> None:
+        pass
+
+
+class BetaModelSoftMax(BaseSimulator):
+    def __init__(self, lr_own, lr_partner, n_choices, beta):
+        self.params = np.ones((n_choices, 2))
+        self.lr_own = lr_own
+        self.lr_partner = lr_partner
+        self.beta = beta
+
+    def make_choice(self):
+        values = self.params[:, 1] / self.params.sum(axis=1)
+        # Calculate the probability of each action using the softmax function.
+        choice_prob = softmax(values * self.beta)
+        # Randomly select an action based on its probability.
+        return np.random.choice(len(self.params), p=choice_prob)
+
+    def learn_from_own(self, choice: int, reward: int) -> None:
+        self.params[choice, reward] += self.lr_own
+
+    def learn_from_partner(self, choice: int, reward: float) -> None:
+        self.params[choice, reward] += self.lr_partner
