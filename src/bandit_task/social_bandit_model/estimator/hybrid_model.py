4b6fe446f8824fd536fce79e8c342be9865054c1
Gota Morishita <optimistic.gota@gmail.com>
2024-08-15 21:21:44 +1000

    implement forgetful hybrid model

--- src/bandit_task/social_bandit_model/estimator/hybrid_model.py
+++ src/bandit_task/social_bandit_model/estimator/hybrid_model.py
@@ -93,6 +93,96 @@ class RewardActionHybridMLE(MLEstimator):
         return LinearConstraint(A, lb, ub)
 
 
+class ForgetfulRewardActionHybridMLE(MLEstimator):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def neg_ll(self, params: Sequence[int | float]) -> float:
+        (
+            lr_for_q_values,
+            lr_for_action_values,
+            forget_rate,
+            beta,
+            weights_for_values,
+        ) = params
+
+        # Initialize all the kind values matrix with 1/2s
+        q_values = np.ones((len(self.your_choices), self.num_choices)) / 2
+        action_values = np.ones((len(self.your_choices), self.num_choices)) / 3
+        n_trials = len(self.your_choices)
+
+        # For each trial, calculate delta and update Q-values
+        for t in range(1, n_trials):
+            current_partner_choice = self.partner_choices[
+                t - 1
+            ]  # Choice made at time t
+            current_partner_reward = self.partner_rewards[
+                t - 1
+            ]  # Reward received at time t
+            delta_t = current_partner_reward - q_values[t - 1, current_partner_choice]
+
+            # Q-value update
+            q_values[t, current_partner_choice] = (
+                q_values[t - 1, current_partner_choice] + lr_for_q_values * delta_t
+            )
+            # For actions not taken, Q-values remain the same
+            for unchosen_choice in range(self.num_choices):
+                if unchosen_choice != current_partner_choice:
+                    q_values[t, unchosen_choice] = 0.5 * forget_rate + q_values[
+                        t - 1, unchosen_choice
+                    ] * (1 - forget_rate)
+
+            # Action value update
+            action_values[t, current_partner_choice] = action_values[
+                t - 1, current_partner_choice
+            ] + lr_for_action_values * (
+                1 - action_values[t - 1, current_partner_choice]
+            )
+
+            # For actions not taken, action values are updated
+            for unchosen_choice in range(self.num_choices):
+                if unchosen_choice != current_partner_choice:
+                    action_values[t, unchosen_choice] = action_values[
+                        t - 1, unchosen_choice
+                    ] + lr_for_action_values * (
+                        0 - action_values[t - 1, unchosen_choice]
+                    )
+
+        # Calculate choice probabilities using softmax function
+        combined_values = q_values * weights_for_values + action_values * (
+            1 - weights_for_values
+        )
+        choice_prob = softmax(combined_values * beta, axis=1)
+
+        # Calculate negative log-likelihood using your own choices not partners!
+        chosen_prob = choice_prob[np.arange(1, n_trials), self.your_choices[:-1]]
+        nll = -np.log(chosen_prob + 1e-8).sum()
+
+        return nll
+
+    def initialize_params(self) -> np.ndarray:
+        init_lr_for_q_values = np.random.beta(2, 2)
+        init_lr_for_action_values = np.random.beta(2, 2)
+        init_forget_rate = np.random.beta(2, 2)
+        init_beta = np.random.gamma(2, 0.333)
+        init_weights = np.random.beta(2, 2)
+        return np.array(
+            [
+                init_lr_for_q_values,
+                init_lr_for_action_values,
+                init_forget_rate,
+                init_beta,
+                init_weights,
+            ]
+        )
+
+    def constraints(self):
+        A = np.eye(5)
+        lb = np.array([0, 0, 0, 0, 0])
+        ub = [1, 1, 1, np.inf, 1]
+        return LinearConstraint(A, lb, ub)
+
+
 class RewardImmediateActionHybridMLE(MLEstimator):
     def __init__(self) -> None:
         """
@@ -209,6 +299,19 @@ class HierarchicalBayesianRewardActionHybrid(HierarchicalEstimator):
             "PR": reshaped_partner_rewards.astype(int).tolist(),
         }
         return stan_data
+
+
+class HierarchicalBayesianForgetfulRewardActionHybrid(
+    HierarchicalBayesianRewardActionHybrid
+):
+    def __init__(self):
+        super().__init__()
+        module_path = os.path.dirname(__file__)
+        self.stan_file = os.path.join(
+            module_path,
+            "stan_files/hierarchical_social_forgetful_action_q_learning_without_rewards.stan",
+        )
+        self.group2ind = None
 
 
 class HierarchicalBayesianRewardImmediateActionHybrid(HierarchicalEstimator):
--- src/bandit_task/social_bandit_model/simulator/hybrid_model.py
+++ src/bandit_task/social_bandit_model/simulator/hybrid_model.py
@@ -102,6 +102,117 @@ class RewardActionHybridSimulator:
                 ] + self.lr_for_action * (0 - self.action_values[unchosen_choice])
 
 
+class ForgetfulRewardActionHybridSimulator:
+    """
+    Implements a reward and action hybrid learning simulator with a softmax action selection strategy.
+
+    Attributes
+    ----------
+    lr_for_reward : float
+        The learning rate used to update Q values.
+    lr_for_action : float
+        The learning rate used to update action values.
+    forget_rate : float
+        The forgetfulness rate
+    beta : float
+        A temperature parameter
+    weights_for_value: float
+        Weights for q values
+    q_values : Sequence[float]
+        A numpy array storing reward values for each action.
+    action_values : Sequence[float]
+        A numpy array storing action values for each action.
+    """
+
+    def __init__(
+        self,
+        lr_for_reward: float,
+        lr_for_action: float,
+        forget_rate: float,
+        beta: float,
+        weights_for_value: float,
+        initial_q_values: Sequence[float],
+        initial_action_values: Sequence[float],
+    ) -> None:
+        """
+        Initialize the ActionSoftmaxSimulator with learning rate, beta parameter, and initial action values.
+
+        Parameters
+        ----------
+        lr_for_reward : float
+            The learning rate used to update Q values.
+        lr_for_action : float
+            The learning rate used to update action values.
+        forget_rate : float
+            The forgetfulness rate
+        beta : float
+            A temperature parameter
+        weights_for_value: float
+            Weights for q values
+        initial_q_values : ndarray
+            Initial values for q values.
+        initial_action_values : ndarray
+            Initial values for action values.
+        """
+        super().__init__()
+        self.lr_for_reward = lr_for_reward
+        self.lr_for_action = lr_for_action
+        self.forget_rate = forget_rate
+        self.beta = beta
+        self.weights_for_values = weights_for_value
+        self.initial_q_values = initial_q_values
+        self.q_values = np.array(initial_q_values)
+        self.action_values = np.array(initial_action_values)
+
+    def make_choice(self) -> int:
+        """
+        Make a choice (i.e., select an action) based on the action values and the softmax policy.
+
+        Returns
+        -------
+        int
+            The index of the selected action.
+        """
+        combined_values = (
+            self.weights_for_values * self.q_values
+            + (1 - self.weights_for_values) * self.action_values
+        )
+        # Calculate the probability of each action using the softmax function.
+        choice_prob = softmax(combined_values * self.beta)
+        # Randomly select an action based on its probability.
+        return np.random.choice(len(self.action_values), p=choice_prob)
+
+    def learn(self, partner_choice: int, partner_reward: int) -> None:
+        """
+        Update the action value for the partner's choice
+
+        Parameters
+        ----------
+        partner_choice : int
+            The index of the partner's choice
+        """
+        # Update the q values for the partner's choice
+        self.q_values[partner_choice] = self.q_values[
+            partner_choice
+        ] + self.lr_for_reward * (partner_reward - self.q_values[partner_choice])
+
+        # Update the action value for the partner's choice
+        self.action_values[partner_choice] = self.action_values[
+            partner_choice
+        ] + self.lr_for_action * (1 - self.action_values[partner_choice])
+
+        for unchosen_choice in range(len(self.action_values)):
+            if unchosen_choice != partner_choice:
+                self.action_values[unchosen_choice] = self.action_values[
+                    unchosen_choice
+                ] + self.lr_for_action * (0 - self.action_values[unchosen_choice])
+
+                self.q_values[unchosen_choice] = (
+                    self.initial_q_values[unchosen_choice] * self.forget_rate
+                    + (1 - self.forget_rate) * self.q_values[unchosen_choice]
+                )
+
+
 class RewardImmediateActionHybridSimulator:
     """
     Attributes
