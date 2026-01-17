77ebe58d56f1be352a74751031581ece9d963677
Gota Morishita <optimistic.gota@gmail.com>
2026-01-17 11:42:18 +1100

    remove unused files

--- src/comp_model/data/__init__.py
+++ src/comp_model/data/__init__.py
@@ -1,4 +1,3 @@
 from .types import Trial, Block, SubjectData, StudyData
-from .stan_pack import pack_stan_data
 
-__all__ = ["Trial", "Block", "SubjectData", "StudyData", "pack_stan_data"]
+__all__ = ["Trial", "Block", "SubjectData", "StudyData"]
