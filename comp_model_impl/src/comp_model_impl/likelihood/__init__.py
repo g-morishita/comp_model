8cea985a3763b297a70f5525b9bcbbf54d8f9f6c
Gota Morishita <optimistic.gota@gmail.com>
2026-01-25 21:35:34 +1100

    add init

--- comp_model_impl/src/comp_model_impl/likelihood/__init__.py
+++ comp_model_impl/src/comp_model_impl/likelihood/__init__.py
@@ -1,3 +1,3 @@
-from .replay import loglike_subject, loglike_study_independent
+from .event_log_replay import loglike_subject, loglike_study_independent
 
 __all__ = ["loglike_subject", "loglike_study_independent"]
